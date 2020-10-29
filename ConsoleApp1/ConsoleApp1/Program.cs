4bbcb9f8c5e926bee5d5501125b483ad06a6d29d
rbabington-nnt <70962282+rbabington-nnt@users.noreply.github.com>
2020-10-29 15:09:58 +0000

    Added OutputMessage function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,13 +4,20 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        static void OutputMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
         /// <summary>
         /// This is a classic test program, that prints hello world
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OutputMessage();
+            
+            Console.ReadLine();
         }
     }
 }
