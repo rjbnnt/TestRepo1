6b7250c6504d62fecba2a7a3bee3831164518abe
rbabington-nnt <70962282+rbabington-nnt@users.noreply.github.com>
2020-10-29 15:06:26 +0000

    updated comment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ namespace ConsoleApp1
     class Program
     {
         /// <summary>
-        /// This is a test program
+        /// This is a classic test program, that prints hello world
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
