ae1363534233799d2ae0fa1e1206f16f1c4c72b9
rbabington-nnt <70962282+rbabington-nnt@users.noreply.github.com>
2020-10-29 15:02:25 +0000

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// This is a test program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
